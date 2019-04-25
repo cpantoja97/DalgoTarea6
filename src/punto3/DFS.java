821543a93774f2da1a3455b5e749745c4553d255
DivierRomero <46934336+DivierRomero@users.noreply.github.com>
2019-04-25 12:12:34 -0500

    x

--- src/punto3/DFS.java
+++ src/punto3/DFS.java
@@ -1,7 +1,16 @@
 package punto3;
 
+import java.util.LinkedList;
+
 public class DFS {
-	public void verificarCiclos(){
-		
+	private int actual;
+	private String recorridoTopologico= "";
+	private LinkedList<Integer> recorridos = new LinkedList<Integer>();
+	
+	public void verificarCiclos(Graph grafo){
+		int grafoA[][] = grafo.getWeights();
+				for (int i = 0; i < grafoA.length; i++) {
+					
+				}
 	}
 }
